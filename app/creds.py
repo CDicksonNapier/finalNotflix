c26f7f2726bce092545e79fbb5e61bb69330186e
Chris Dickson <113544017+CDicksonNapier@users.noreply.github.com>
2022-11-26 11:03:48 +0000

    new additions

--- app/creds.py
+++ app/creds.py
@@ -7,8 +7,7 @@ load_dotenv()
 SECRET_KEY = os.getenv('SECRET_KEY')
 API_KEY = os.getenv('API_KEY')
 
-BASE_URL = "https://api.themoviedb.org/3/movie/popular?api_key=" +API_KEY
-TV_URL =" https://api.themoviedb.org/3/tv/top_rated?api_key=" +API_KEY
-
+BASE_URL = "https://api.themoviedb.org/3/movie/popular?api_key=" 
+TV_URL =" https://api.themoviedb.org/3/tv/top_rated?api_key=" 
 
 
