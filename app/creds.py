4c4387c70dbf56b976eb4f1f99fb5f058e2584c4
Chris Dickson <113544017+CDicksonNapier@users.noreply.github.com>
2022-11-26 13:08:05 +0000

    Added config Vars

--- app/creds.py
+++ app/creds.py
@@ -4,8 +4,8 @@ import os
 load_dotenv()
 
 
-SECRET_KEY = 'SECRET_KEY'
-API_KEY = '4b96cadadd63dfa1342c3b257a7f46d0'
+SECRET_KEY = os.getenv('SECRET_KEY')
+API_KEY =  os.getenv('API_KEY')
 
 BASE_URL = "https://api.themoviedb.org/3/movie/popular?api_key=" +API_KEY
 TV_URL =" https://api.themoviedb.org/3/tv/top_rated?api_key=" +API_KEY
