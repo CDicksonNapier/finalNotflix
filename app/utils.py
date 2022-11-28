8dc8c1bd55a2bf07eb9d68acefd8a12ed31a4ad1
Chris Dickson <113544017+CDicksonNapier@users.noreply.github.com>
2022-11-28 08:51:03 +0000

    quick fixes
    remnoved all add to watchlists stuff just now will re add later

--- app/utils.py
+++ app/utils.py
@@ -1,13 +1,13 @@
-# Importing the models and flask_login to pull the current User 
-from app.models import User, WatchList
-from flask_login import current_user
-# Defining the Wath list and appending the user selections to it 
-def watch_list(user_id):
-    shows = WatchList.query.filter_by(user_id=user_id).all()
-    show_list, movie_list = [], []
-    for item in shows:
-        if item.show:
-            show_list.append(item.show)
-        else:
-            movie_list.append(item.movie)
-    return [movie_list, show_list]
+# # Importing the models and flask_login to pull the current User 
+# from app.models import User, WatchList
+# from flask_login import current_user
+# # Defining the Wath list and appending the user selections to it 
+# def watch_list(user_id):
+#     shows = WatchList.query.filter_by(user_id=user_id).all()
+#     show_list, movie_list = [], []
+#     for item in shows:
+#         if item.show:
+#             show_list.append(item.show)
+#         else:
+#             movie_list.append(item.movie)
+#     return [movie_list, show_list]
